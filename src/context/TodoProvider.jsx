d13fd912636e52b8b2e1eb175a7cd6c8360c5db9
Syarif Muhammad Alhaiza <alqadrihaiza@gmail.com>
2023-11-04 20:28:51 +0700

    FINAL To Do

--- src/context/TodoProvider.jsx
+++ src/context/TodoProvider.jsx
@@ -4,8 +4,9 @@ export const TodoContext = createContext();
 
 function TodoProvider({ children }) {
   const [todos, setTodos] = useState([
-    { id: 1, value: "Make Todo App", status: false },
-    { id: 2, value: "Learn React", status: false },
+    { id: 1, value: "Melupakan dia", status: false },
+    { id: 2, value: "Move On", status: false },
+    { id: 2, value: "Cari Yang Baru", status: false },
   ]);
 
   const [todoInput, setTodoInput] = useState("");
